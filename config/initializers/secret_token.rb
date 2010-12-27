e151991694a460fda4381c227bd34be6bf108a82
Ryan Weald <ryan@weald.com>
2010-12-27 15:39:05 -0800

    Created the base application that contains the custom gemfile and the README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClearanceDatamapper::Application.config.secret_token = '2adae9b1d3d80291cec573cb308affb754d770fde1677c717b95a707137075a50c8b8cc58cba150a8e4ada4be6c8fd55c07fde73602a41402e901c9eb738748d'
