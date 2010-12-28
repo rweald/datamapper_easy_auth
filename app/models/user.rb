187219afecad5436241bf05d43a71e461b2bfdc5
Ryan Weald <ryan@weald.com>
2010-12-27 17:06:29 -0800

    Created the first part of the user model class and accompanying test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,36 @@
 class User
-
   include DataMapper::Resource
 
+  # instance variable to hold unhashed password temporarily
+  attr_accessor :password
+
+  # The basic properties
   property :id, Serial
+  property :email, String, :required => true, :format => :email_address
+  property :salt, String
+  property :hashed_password, String
+
+  # properties for email verification
+  property :verified, Boolean, :default => false
+  property :verification_token, String, :required => true
+
+  # properties for password reset
+  property :password_change_token , String
+  property :token_expiration, DateTime
 
 
+  # hooks to add values and perform necessary operations before insert to db
+  before :valid?, :create_verification_token
+
+  # Method that will create a verification token that
+  # can be emailed out to ensure a valid email address
+  def create_verification_token
+    string = "#{DateTime.now().to_i()}" + self.id.to_s() + self.salt.to_s()
+    self.verification_token = generate_hash(string)
+  end
+
+  # Method that will generate a hash
+  def generate_hash(string)
+    Digest::SHA1.hexdigest(string)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @new_user = User.new :email => "test@test.com", :password => "test"
+  end
+  it "should validate presence of email" do
+    @new_user.email = "test@test.com"
+    @new_user.save.should_not == false
+
+    @new_user.email = nil
+    @new_user.save.should_not == true
+    
+  end
+  
+  it "should generate salt upon save" do
+    @new_user.save
+    @new_user.salt.should_not be_nil
+  end
+  
+  it "should hash the password upon save" do
+    @new_user.save
+    @new_user.hashed_password.should_not be_nil
+  end
 end
