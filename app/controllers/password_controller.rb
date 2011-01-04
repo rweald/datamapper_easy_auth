cb1d10f90c6dba49366ef649af37307446ea14e2
Ryan Weald <ryan@weald.com>
2011-01-04 15:59:28 -0800

    Implemented the :new method for password_controller and accompanying tests. method will determine if it should render the new password form or the invalid token page

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -2,6 +2,11 @@ class PasswordController < ApplicationController
   def new
     # get the user from the id in the url
     current_user = User.get(params[:user_id])
-    current_user.password_change_token_valid? :token => params[:token] 
+    if current_user.password_change_token_valid? :token => params[:token] 
+      render :action => "new"
+    else
+      flash[:error] = "Your password reset token has expired please request another one"
+      render :action => "invalid_new"
+    end
   end
 end
--- spec/controllers/password_controller_spec.rb
+++ spec/controllers/password_controller_spec.rb
@@ -2,27 +2,38 @@ require 'spec_helper'
 
 describe PasswordController do
   describe "GET new" do
+    # set up the user and create the password reset token
+    before(:each) do
+      @myUser = mock()
+      User.stubs(:get).returns(@myUser)
+    end
+  
     context "token is invalid" do
-            
+      before do
+        @myUser.expects(:password_change_token_valid?).returns(false)
+      end  
       it "should set flash[:error]" do
+        get :new, :token => "bad value", :user_id  => 1 
         flash[:error].should_not be_nil
-        get :new, :token => "bad value", :email => "test@test.com" 
       end
       
-      it "should redirect to error page" do
-        get :new, :token => "bad value", :email => "test@test.com" 
-        response.should redirect_to(:controller => "password", :action => "bad_token")
+      it "should render error page" do
+        get :new, :token => "bad value", :user_id  => 1 
+        response.should render_template("invalid_new")
       end
     end
       
     context "token is valid" do
+      before do
+        @myUser.expects(:password_change_token_valid?).returns(true)
+      end
       it "should not set the flash" do
-        get :new
+        get :new, :token => "bad value", :user_id  => 1 
         flash[:error].should be_nil
       end
       it "should render the view" do
-        get :new
-        response.should be_success
+        get :new, :token => "bad value", :user_id  => 1 
+        response.should render_template("new")
       end
     end
   end
